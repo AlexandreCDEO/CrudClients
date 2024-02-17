71f46515de6c97cb6f094dac119ff1d3dd363694
AlexandreCDEO <acdo13031991@gmail.com>
2024-02-16 22:48:11 -0300

    create entity client

--- src/test/java/com/alexandre/oliveira/crudclients/CrudClientsApplicationTests.java
+++ src/test/java/com/alexandre/oliveira/crudclients/CrudClientsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CrudClientsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
